4fa673e652016441e1457e16d259c35f348233b1
Zeydax <phil3001@hotmail.com>
2017-12-20 10:49:35 +0100

    Revert "Re-organisation"
    This reverts commit 5a037505ec6d356857dc8e956bbcff9da4856749.

--- Eval 5/Application_CIAChat/SocketUDP.h
+++ Eval 5/Application_CIAChat/SocketUDP.h
@@ -1,14 +1,9 @@
-//
-// Created by Doublon on 19/12/2017.
-//
-
-#ifndef CPP_CHAT_SOCKETUDP_H
-#define CPP_CHAT_SOCKETUDP_H
+#ifndef APPLICATION_CIACHAT_SOCKETUDP_H
+#define APPLICATION_CIACHAT_SOCKETUDP_H
 
 #include <stdio.h>
 #include <stdlib.h> /* pour exit */
 #include <string.h> /* pour memcpy */
-#include <string>
 #include <sys/types.h>
 #include <sys/socket.h> /* pour les types de socket */
 #include <sys/time.h> /* pour les types de socket */
@@ -17,54 +12,21 @@
 #include <netinet/in.h> /* pour la conversion adresse reseau->format dot ainsi que le conversion format local/format reseau */
 #include <netinet/tcp.h> /* pour la conversion adresse reseau->format dot */
 #include <arpa/inet.h> /* pour la conversion adresse reseau->format dot */
-
-#define SEPARATOR "#"
-#define ADRESSEIP "192.168.0.3"
-#define PORTUPD 30051
+//#include <Winsock2.h>
+//#include <ws2tcpip.h>
 
 #define MAXSTRING 200
 
-class SocketUdp
-{
-    protected :
-        int hSocket=-1; /* Handle de la socket */
-        struct hostent * infosHost;
-        struct in_addr adresseIPUdp;
-        struct sockaddr_in adresseSocketUdp;
-        unsigned int tailleSockaddr_in;
+int CreateSocketUDP();
+void GetInfosHost(struct hostent ** infosHost, struct in_addr * adresseIP);
+void PrepareSockAddrIn(struct sockaddr_in *adresseSocketServeur, unsigned int *tailleSockaddr_in, int Port);
+void BindSocket(int hSocket, struct sockaddr_in adresseSocketServeur, unsigned int tailleSockaddr_in);
+void SettingUpSocket(int hSocket, struct ip_mreq mreq, struct sockaddr_in adresseSocketServeur, unsigned int tailleSockaddr_in);
+void CloseSocketUDP(int hSocket);
 
-    public :
-        /*************CONSTRUCTOR*************/
-        SocketUdp(int hsocket , hostent **infohost);
-        SocketUdp(const SocketUdp& s);
-        ~SocketUdp();
+void EnvoyerMessageUDP(char* Message, int hSocket, struct sockaddr_in adresseSocketServeur, unsigned int tailleSockaddr_in);
+void RecevoirMessageUDP(int hSocket, struct sockaddr_in adresseSocketClient, unsigned int tailleSockaddr_in);
 
 
-    /*************METHODE*************/
-        void EnvoyerMessageUDP();
-        void RecevoirMessageUPD();
 
-        /*************GETTER/SETTER*************/
-        int getHSocket() const;
-
-        void setHSocket(int hSocket);
-
-        const sockaddr_in &getAdresseSocketUdp() const;
-
-        void setAdresseSocketUdp();
-
-        hostent *getInfosHost() const;
-
-        void setInfosHost(hostent *infosHost);
-
-        const in_addr &getAdresseIPUdp() const;
-
-        void setAdresseIPUdp();
-
-        unsigned int getTailleSockaddr_in() const;
-
-        void setTailleSockaddr_in(unsigned int tailleSockaddr_in);
-
-};
-
-#endif //CPP_CHAT_SOCKETUDP_H
+#endif //APPLICATION_CIACHAT_SOCKETUDP_H
--- Eval 5/Cpp/Cpp_Chat/cmake-build-debug/cmake_install.cmake
+++ Eval 5/Cpp/Cpp_Chat/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /cygdrive/d/GitHub/Reseaux/Eval 5/Application_CIAChat
+# Install script for directory: /cygdrive/d/GitHub/Reseaux/Eval 5/Cpp/Cpp_Chat
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -35,5 +35,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/cygdrive/d/GitHub/Reseaux/Eval 5/Application_CIAChat/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/cygdrive/d/GitHub/Reseaux/Eval 5/Cpp/Cpp_Chat/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
