5a037505ec6d356857dc8e956bbcff9da4856749
Zeydax <phil3001@hotmail.com>
2017-12-20 10:46:21 +0100

    Re-organisation

--- Eval 5/Application_CIAChat/SocketUDP.h
+++ Eval 5/Application_CIAChat/SocketUDP.h
@@ -1,9 +1,14 @@
-#ifndef APPLICATION_CIACHAT_SOCKETUDP_H
-#define APPLICATION_CIACHAT_SOCKETUDP_H
+//
+// Created by Doublon on 19/12/2017.
+//
+
+#ifndef CPP_CHAT_SOCKETUDP_H
+#define CPP_CHAT_SOCKETUDP_H
 
 #include <stdio.h>
 #include <stdlib.h> /* pour exit */
 #include <string.h> /* pour memcpy */
+#include <string>
 #include <sys/types.h>
 #include <sys/socket.h> /* pour les types de socket */
 #include <sys/time.h> /* pour les types de socket */
@@ -12,21 +17,54 @@
 #include <netinet/in.h> /* pour la conversion adresse reseau->format dot ainsi que le conversion format local/format reseau */
 #include <netinet/tcp.h> /* pour la conversion adresse reseau->format dot */
 #include <arpa/inet.h> /* pour la conversion adresse reseau->format dot */
-//#include <Winsock2.h>
-//#include <ws2tcpip.h>
+
+#define SEPARATOR "#"
+#define ADRESSEIP "192.168.0.3"
+#define PORTUPD 30051
 
 #define MAXSTRING 200
 
-int CreateSocketUDP();
-void GetInfosHost(struct hostent ** infosHost, struct in_addr * adresseIP);
-void PrepareSockAddrIn(struct sockaddr_in *adresseSocketServeur, unsigned int *tailleSockaddr_in, int Port);
-void BindSocket(int hSocket, struct sockaddr_in adresseSocketServeur, unsigned int tailleSockaddr_in);
-void SettingUpSocket(int hSocket, struct ip_mreq mreq, struct sockaddr_in adresseSocketServeur, unsigned int tailleSockaddr_in);
-void CloseSocketUDP(int hSocket);
+class SocketUdp
+{
+    protected :
+        int hSocket=-1; /* Handle de la socket */
+        struct hostent * infosHost;
+        struct in_addr adresseIPUdp;
+        struct sockaddr_in adresseSocketUdp;
+        unsigned int tailleSockaddr_in;
 
-void EnvoyerMessageUDP(char* Message, int hSocket, struct sockaddr_in adresseSocketServeur, unsigned int tailleSockaddr_in);
-void RecevoirMessageUDP(int hSocket, struct sockaddr_in adresseSocketClient, unsigned int tailleSockaddr_in);
+    public :
+        /*************CONSTRUCTOR*************/
+        SocketUdp(int hsocket , hostent **infohost);
+        SocketUdp(const SocketUdp& s);
+        ~SocketUdp();
 
 
+    /*************METHODE*************/
+        void EnvoyerMessageUDP();
+        void RecevoirMessageUPD();
 
-#endif //APPLICATION_CIACHAT_SOCKETUDP_H
+        /*************GETTER/SETTER*************/
+        int getHSocket() const;
+
+        void setHSocket(int hSocket);
+
+        const sockaddr_in &getAdresseSocketUdp() const;
+
+        void setAdresseSocketUdp();
+
+        hostent *getInfosHost() const;
+
+        void setInfosHost(hostent *infosHost);
+
+        const in_addr &getAdresseIPUdp() const;
+
+        void setAdresseIPUdp();
+
+        unsigned int getTailleSockaddr_in() const;
+
+        void setTailleSockaddr_in(unsigned int tailleSockaddr_in);
+
+};
+
+#endif //CPP_CHAT_SOCKETUDP_H
---
+++ Eval 5/Application_CIAChat/cmake-build-debug/CMakeFiles/..dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/..dir/main.cpp.o"
+  "CMakeFiles/..dir/SocketUdp.cpp.o"
+  "..pdb"
+  "..exe"
+  "lib..dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/..dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
