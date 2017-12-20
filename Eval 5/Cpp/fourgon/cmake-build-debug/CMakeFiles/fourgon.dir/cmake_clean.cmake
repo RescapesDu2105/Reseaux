c49a729a339d0d2b837ec3cc6cf452553bacc208
Zeydax <phil3001@hotmail.com>
2017-12-20 10:48:49 +0100

    Revert "2"
    This reverts commit 8938c3ecd51fc89cf792202713b857c667abbad2.

---
+++ Eval 5/Cpp/fourgon/cmake-build-debug/CMakeFiles/fourgon.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fourgon.dir/main.c.o"
+  "fourgon.pdb"
+  "fourgon.exe"
+  "libfourgon.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/fourgon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
